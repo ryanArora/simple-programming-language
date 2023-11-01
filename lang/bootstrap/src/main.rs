161f2aaf3f2bec7afa8f21f99307ae995dec3327
Ryan Arora <arora.ryan@gmail.com>
2023-11-01 14:05:57 -0400

    lang bootstrap: command line parser

--- lang/bootstrap/src/main.rs
+++ lang/bootstrap/src/main.rs
@@ -1,3 +1,15 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+struct Args {
+    input_file: PathBuf,
+    #[arg(short)]
+    output_file: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("input_file: {}", args.input_file.display());
+    println!("output_file: {}", args.output_file.display());
 }
